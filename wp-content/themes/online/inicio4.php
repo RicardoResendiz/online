3e86b7e6e9c01e56e228d654e2b1523cc759dd30
UVP Online <programador6@uvp.mx>
2019-06-24 16:12:02 -0500

    Update inicio4.php

--- wp-content/themes/online/inicio4.php
+++ wp-content/themes/online/inicio4.php
@@ -1,1 +1,378 @@
+<?php /* Template Name:INICIO3*/ ?>
+<?php get_header(); ?>
+<section class="banner-seleccion-grado">
+	<div class="header-grado">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="banner_content" id="inicio3-header">
+						<p style="line-height: .5; margin: 0px;"><strong>ONLINE</strong><i class="fas fa-users"></i><span>2450</span></p>
+						<h2 class="text-uppercase poppins-bold title-banner" style="line-height: 1;">
+							LICENCIATURA ONLINE EN INGENIERIA INDUSTRIAL
+						</h2>
 						<h3 class="text-uppercase signika" style="margin-bottom: 20px;">
+							ACUERDO 20180806
+						</h3>
+						<a href="#" class="btn-inicio3-header">SOLICITA INFORMES</a>
+						<a href="#" class="btn-inicio3-header">INSCRÍBETE AHORA</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="feature_area section_gap_top">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 col-xl-4">
+				<div class="single_feature">
+					<div class="icon text-center">
+						<i class="far fa-clock"></i>
+					</div>
+					<div class="desc">
+						<p class="mt-3 mb-2 text-center poppins-bold">ADMINISTRA TU TIEMPO</p>
+						<p class="text-center">Puedes acceder a nuestra plataforma de estudio para estudiar en tus ratos libres desde cualquier lugar.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-xl-4">
+				<div class="single_feature">
+					<div class="icon text-center">
+						<i class="fas fa-medal"></i>
+					</div>
+					<div class="desc">
+						<p class="mt-3 mb-2 text-center poppins-bold">LOGRA TU OBJETIVO</p>
+						<p class="text-center">Termina tu licenciatura en un período de 3 años y 8 meses y obtén el mismo título profesional de sistema escolarizado.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-xl-4">
+				<div class="single_feature">
+					<div class="icon text-center">
+						<i class="fas fa-user-tie"></i>
+					</div>
+					<div class="desc">
+						<p class="mt-3 mb-2 text-center poppins-bold">TUTORES ONLINE</p>
+						<p class="text-center">Nuestros tutores se encuentran disponibles para responder a tus dudas en los horarios establecidos o por correo electrónico.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="paral paralsec1">
+  <div class="container">
+    <div id="parallax-tab" class="row">
+      <div class="col-12 col-md-12 col-xl-12 display-2">
+        <h1 class="signika-bold">LICENCIATURA ONLINE EN INGENIERIA INDUSTRIAL</h2>	
+      </div>
+      <div class="col-12 col-md-12 col-xl-12">
+      	<p id="inicio2-parallax">La Licenciatura Online en Ingenieria Industrial forma profesionales capaces de gestionar de manera estratégica cualquier tipo de organización con habilidades necesarias para dirigir y tomar decisiones proactivas en los ámbitos laborales.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<section class="feature_area section_gap_top">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-6 col-xl-3">
+				<div class="single_feature">
+					<div class="icon text-center">
+						<i class="fas fa-user-tie"></i>
+					</div>
+					<div class="desc">
+						<p class="mt-3 mb-2 text-center poppins-bold">TUTORES</p>
+						<p class="text-center">Se te asignarán tutores de materia y de carrera para darte asesoría en el transcurso de toda tu licenciatura.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-xl-3">
+				<div class="single_feature">
+					<div class="icon text-center">
+						<i class="fas fa-dollar-sign"></i>
+					</div>
+					<div class="desc">
+						<p class="mt-3 mb-2 text-center poppins-bold">COSTOS</p>
+						<p class="text-center">Inscripción Única: $1,300 Colegiatura Mensual: $3,050</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-xl-3">
+				<div class="single_feature">
+					<div class="icon text-center">
+						<i class="far fa-credit-card"></i>
+					</div>
+					<div class="desc">
+						<p class="mt-3 mb-2 text-center poppins-bold">PAGOS</p>
+						<p class="text-center">Ponemos a tu disposición nuestra plataforma Online para que realices tus pagos de forma rápida y segura.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-xl-3">
+				<div class="single_feature">
+					<div class="icon text-center">
+						<i class="fas fa-handshake"></i>
+					</div>
+					<div class="desc">
+						<p class="mt-3 mb-2 text-center poppins-bold">CONVENIOS</p>
+						<p class="text-center">Pregunta por nuestras becas por convenio y por rendimiento académico.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<h1 class="signika-bold-black text-heading title_color">ESTAS SON ALGUNAS DE LAS INSTITUCIONES Y EMPRESAS CON LAS QUE LA UVP TIENE CONVENIOS:</h1>
+		<style>
+			.convenios{
+				text-align: center;
+				border: 1px solid #CCCCCC;
+				border-radius: 5px;
+				padding: 15px;
+			}
+			.padre-convenios{
+				flex-grow: 1;
+				padding: 15px;
+				max-width: 20%;
+				min-width: 20%;
+			}
+		</style>
+		<div id="repres" class="row" style="margin-top: 2%;">
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+			<div class="padre-convenios">
+				<div class="convenios">
+					<img class="img-fluid" src=" <?php echo get_stylesheet_directory_uri()?>/images/ado.png" alt="">
+				</div>
+			</div>
+		</div>
+		<h1 class="signika-bold-black text-heading title_color">PLAN DE ESTUDIOS</h1>
+		<div class="row">
+			<div class="col-md-9">
+				<h4>Nuestro plan de estudios esta dividido en 11 cuatrimestres que conforman la licenciatura, asignándose una materia a la vez hasta terminar el plan de estudios.</h4>
+			</div>
+			<div class="col-md-3">
+				<span><i class="fas fa-users"></i></span><a href="#" class="btn-plan mt-3">PLAN DE ESTUDIOS COMPLETO</a>
+			</div>
+		</div>
+		<style>
+			.lev1{
+				min-height: 280px;
+			}
+			.lev2{
+				min-height: 200px;
+			}
+			.lev3{
+				min-height: 135px;
+			}
+			.lev4{
+				min-height: 100px;
+			}
+		</style>
+		<div class="section-top-border">
+			<div class="row">
+				<div id="admin" class="col-md-4 col-planes ">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">ADMINISTRACIÓN</h4>
+						</div>
+						<div class="planes lev1">
+							<ul>
+								<li>Introducción a la Administración</li>
+								<li>Proceso Administrativo</li>
+								<li>Administración de Recursos Humanos</li>
+								<li>Administración estratégica</li>
+								<li>Liderazgo Social</li>
+								<li>Administración de calidad</li>
+								<li>Habilidades Directivas</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes ">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">ECONOMÍA</h4>
+						</div>
+						<div class="planes lev1">
+							<ul>
+								<li>Introducción a la economía</li>
+								<li>Microeconomía</li>
+								<li>Macroeconomía</li>
+								<li>Entorno económico mundial</li>
+								<li>Econometría</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes ">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">CONTABILIDAD</h4>
+						</div>
+						<div class="planes lev1">
+							<ul>
+								<li>Contabilidad Financiera</li>
+								<li>Contabilidad de costos</li>
+								<li>Contabilidad para la toma de decisiones</li>
+								<li>Finanzas I</li>
+								<li>Finanzas II</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">MATEMÁTICAS</h4>
+						</div>
+						<div class="planes lev2">
+							<ul>
+								<li>Matemáticas</li>
+								<li>Matemáticas II</li>
+								<li>Estadística</li>
+								<li>Administración de Operaciones y Producción</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">MERCADOTECNIA</h4>
+						</div>
+						<div class="planes lev2">
+							<ul>
+								<li>Introducción a la mercadotecnia</li>
+								<li>Investigación de mercados</li>
+								<li>Conducta del consumidor</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">TECNOLOGÍA DE LA INFORMACIÓN</h4>
+						</div>
+						<div class="planes lev2">
+							<ul>
+								<li>Introducción a la tecnología de la información</li>
+								<li>Administración de los sistemas de información</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">INGLÉS</h4>
+						</div>
+						<div class="planes lev3">
+							<ul>
+								<li>Inglés I</li>
+								<li>Inglés II</li>
+								<li>Inglés III</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">LENGUAJE</h4>
+						</div>
+						<div class="planes lev3">
+							<ul>
+								<li>Habilidades de comunicación </li>
+								<li>Análisis de la información</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">DERECHO</h4>
+						</div>
+						<div class="planes lev3">
+							<ul>
+								<li>Derecho empresarial I</li>
+								<li>Derecho Empresarial II</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">EMPRENDEDORES</h4>
+						</div>
+						<div class="planes lev4">
+							<ul>
+								<li>Emprendedurismo</li>
+								<li>Liderazgo empresarial</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">METODOLOGÍA DE LA INVESTIGACIÓN</h4>
+						</div>
+						<div class="planes lev4">
+							<ul>
+								<li>Metodología de la investigación</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div id="admin" class="col-md-4 col-planes">
+					<div class="div-planes">
+						<div class="planes-header">
+							<h4 class="planes-title">ÉTICA</h4>
+						</div>
+						<div class="planes lev4">
+							<ul>
+								<li>Ética</li>
+							</ul>
+						</div>
